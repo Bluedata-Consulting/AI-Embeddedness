9af11ed8f6dd70a13b5aac643e7cda6db50a25fd
bdc-ujjavalr <114487150+bdc-ujjavalr@users.noreply.github.com>
2025-01-24 10:42:16 +0530

    App Code

---
+++ config/config.py
@@ -0,0 +1,11 @@
+import os
+from pathlib import Path
+
+# Model paths
+sentiment_model_path = "D:\\Working Blue Data\\AI Product\\AI Embedness\\models\\models--cardiffnlp--twitter-roberta-base-sentiment\\snapshots\\daefdd1f6ae931839bce4d0f3db0a1a4265cd50f"
+translation_model_path = "D:\\Working Blue Data\\AI Product\\AI Embedness\\models\\models--facebook--m2m100_418M\\snapshots\\55c2e61bbf05dfb8d7abccdc3fae6fc8512fd636"
+image_model_path = "D:\\Working Blue Data\\AI Product\\AI Embedness\\models\\models--microsoft--resnet-50\\snapshots\\34c2154c194f829b11125337b98c8f5f9965ff19"
+
+# API Keys
+OPENAI_API_KEY = "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"
+GEMINI_API_KEY = "AIzaSyAQMU74h93W8Y5pgvlDRASLGGXxO1_sn0o"
